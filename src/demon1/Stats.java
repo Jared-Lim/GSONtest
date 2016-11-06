8781b98d307b5125ab02538bc6b5368bc7994f19
Jared <jaredlgw@outlook.com>
2016-11-07 03:26:13 +1100

    Added stats class

---
+++ src/demon1/Stats.java
@@ -0,0 +1,15 @@
+package demon1;
+
+public class Stats {
+	private int hp;
+	private int mp;
+	private int strength;
+	private int skill;
+	private int magic;
+	private int speed;
+	private int luck;
+	
+	public String toString(){
+		return "----"+hp+mp+strength+skill+magic+speed+luck;
+	}
+}

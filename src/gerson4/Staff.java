22371293c99055329134baadb2b8f823951c2e82
Jared <jaredlgw@outlook.com>
2016-11-06 18:24:32 +1100

    Added public setter methods

--- src/gerson4/Staff.java
+++ src/gerson4/Staff.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 @SuppressWarnings("unused")
 public class Staff {
-
+	
 	private String name;
 	private int age;
 	private String position;
 	private BigDecimal salary;
 	private List<String> skills;
-
+	
+	public void setName(String name){this.name=name;}
+	public void setAge(int age){this.age=age;}
+	public void setPosition(String position){this.position=position;}
+	public void setSalary(BigDecimal salary){this.salary=salary;}
+	
+	
 }

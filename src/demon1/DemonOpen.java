6c0197cd1f58f53fe6eedbbb08a1d630e6d315f3
Jared <jaredlgw@outlook.com>
2016-11-07 02:38:52 +1100

    Added first bit of filereader

--- src/demon1/DemonOpen.java
+++ src/demon1/DemonOpen.java
@@ -1,13 +1,21 @@
 package demon1;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class DemonOpen {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		GsonBuilder builder = new GsonBuilder();
 		builder.serializeNulls();
 		builder.setPrettyPrinting().serializeNulls();
 	    Gson gson = builder.create();
+	    
+	    Reader reader = new FileReader("demons/Ama1.json");
+	    
+	    
 	}
 }

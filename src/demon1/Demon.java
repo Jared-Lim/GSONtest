85efbed6272b8d1703531c623560a9c69a66a259
Jared <jaredlgw@outlook.com>
2016-11-07 00:00:13 +1100

    Added toString

--- src/demon1/Demon.java
+++ src/demon1/Demon.java
@@ -5,4 +5,11 @@ public class Demon {
 	public String nameJP;
 	public String nameEN;
 	public int level;
+	
+	public String toString(){
+		return "--tribe: \n"+tribe+
+				"--nameJP: \n"+nameJP+
+				"--nameEn: \n"+nameEN+
+				"--level: \n"+level;
+	}
 }

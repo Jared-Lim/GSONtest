ada7a535ab700c6bb46ec9beaa0f8c6d07b94e59
Jared <jaredlgw@outlook.com>
2016-11-07 02:48:08 +1100

    Removed pretty printing because unused

--- src/demon1/DemonOpen.java
+++ src/demon1/DemonOpen.java
@@ -5,14 +5,10 @@ import java.io.IOException;
 import java.io.Reader;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class DemonOpen {
 	public static void main(String[] args) throws IOException {
-		GsonBuilder builder = new GsonBuilder();
-		builder.serializeNulls();
-		builder.setPrettyPrinting().serializeNulls();
-	    Gson gson = builder.create();
+	    Gson gson = new Gson();
 	    
 	    Reader reader = new FileReader("demons/Ama1.json");
 	    

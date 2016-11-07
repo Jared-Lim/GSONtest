3a6e9233947552d444854977000623562306429d
Jared <jaredlgw@outlook.com>
2016-11-08 00:05:59 +1100

    Added parameters

--- src/demon1/Affinities.java
+++ src/demon1/Affinities.java
@@ -1,5 +1,22 @@
 package demon1;
 
 public class Affinities {
-
+	public String physical;
+	public String gun;
+	public String fire;
+	public String ice;
+	public String thunder;
+	public String shock;
+	public String banish;
+	public String curse;
+	public String bind;
+	public String sleep;
+	public String cold;
+	public String confusion;
+	public String poison;
+	
+	@Override
+	public String toString(){
+		return "test";
+	}
 }

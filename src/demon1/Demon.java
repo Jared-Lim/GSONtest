8510d423e250b147165538bfa2ebe40b5ae7ea6b
Jared <jaredlgw@outlook.com>
2016-11-08 01:03:09 +1100

    Realized fusions is not a json so trying to add it into the demon class

--- src/demon1/Demon.java
+++ src/demon1/Demon.java
@@ -1,15 +1,21 @@
 package demon1;
 
+import java.util.ArrayList;
+
 public class Demon {
 	private String tribe;
 	private String nameJP;
 	private String nameEN;
 	private int level;
 	
+	private ArrayList<ArrayList<String>> specialfusion 
+		= new ArrayList<ArrayList<String>>();
+	
 	public String toString(){
 		return    "--tribe: "+tribe+
 				"\n--nameJP: "+nameJP+
 				"\n--nameEn: "+nameEN+
-				"\n--level: "+level;
+				"\n--level: "+level+
+				"\n--FUS: "+specialfusion;
 	}
 }

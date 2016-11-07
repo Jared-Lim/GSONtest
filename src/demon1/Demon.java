a3c1038066f392c298901a1bedd9d64b6a2146c5
Jared <jaredlgw@outlook.com>
2016-11-08 01:26:47 +1100

    Added in fusions to demon, will add more as needed

--- src/demon1/Demon.java
+++ src/demon1/Demon.java
@@ -1,21 +1,20 @@
 package demon1;
 
-import java.util.ArrayList;
-
 public class Demon {
 	private String tribe;
 	private String nameJP;
 	private String nameEN;
 	private int level;
 	
-	private ArrayList<ArrayList<String>> specialfusion 
-		= new ArrayList<ArrayList<String>>();
+	private String[][] fusions;
 	
 	public String toString(){
 		return    "--tribe: "+tribe+
 				"\n--nameJP: "+nameJP+
 				"\n--nameEn: "+nameEN+
 				"\n--level: "+level+
-				"\n--FUS: "+specialfusion;
+				"\n--FUS: "+fusions[0][0]+" + "+fusions[0][1]+" + "+fusions[0][2];
 	}
+	
+	
 }

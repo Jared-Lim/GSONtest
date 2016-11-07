e6c62f83e8c12b7969c7f50bc5647ca831ad73df
Jared <jaredlgw@outlook.com>
2016-11-07 20:24:59 +1100

    FIRST BIT OF TESTING

--- src/cereal/DeserialTest1.java
+++ src/cereal/DeserialTest1.java
@@ -1,5 +1,37 @@
 package cereal;
 
+import java.lang.reflect.Type;
+import java.util.List;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
 public class DeserialTest1 {
 
+}
+
+class Skills{
+	public List<SingleSkill> ListOfSkills; 
+}
+
+class SingleSkill{
+	
+}
+
+class SkillsDeserializer implements JsonDeserializer<Skills>{
+
+	@Override
+	public Skills deserialize(JsonElement arg0, Type arg1, JsonDeserializationContext arg2) throws JsonParseException {
+		// TODO Auto-generated method stub
+		JsonArray jArray = (JsonArray) arg0;
+		
+		
+		
+		
+		return null;
+	}
+	
 }
